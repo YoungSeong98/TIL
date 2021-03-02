addeaff8476dc6370be3d5b171200045532db2b2
임용성 <dydrkfrkfrkf@gmail.com>
2021-03-03 00:33:51 +0900

    20210302

---
+++ 2021년/SW Expert Academy/2027.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+main()
+{
+	int row, column, i;
+
+	for (row = 0; row < 5; row++)
+	{
+		for (column = 0; column < 5; column++)
+		{
+			if (row == column)
+				printf("#");
+			else
+				printf("+");
+		}
+		printf("\n");
+	}
+
+}
