98e3d935f848f028d13aa78fa6736f77b6a16c2e
임용성 <dydrkfrkfrkf@gmail.com>
2021-04-20 02:30:48 +0900

    20210420

--- 2021년/SW Expert Academy/D2/Python/1959.py
+++ 2021년/SW Expert Academy/D2/Python/1959.py
@@ -1,26 +1,4 @@
-def init():
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
-    return N, M, A, B
-
-def swap(A, B):
-    if len(A) > len(B):
-        return B, A
-    return A, B
-
-def calculate(A, B):
-    maxNumber = float('-inf')
-    diff = len(B) - len(A)
-    for i in range(diff+1):
-        tempNumber = 0
-        for j in range(len(A)):
-            tempNumber += A[j] * B[j+i]
-        maxNumber = max(maxNumber, tempNumber)        
-    return maxNumber
-
-for t in range(int(input())):
-    N, M, A, B = init()
-    A, B = swap(A, B)
-    answer = calculate(A, B)
-    print('#{} {}'.format(t+1, answer))
+a = 1
+if a==1:
+    print('1')
+    
